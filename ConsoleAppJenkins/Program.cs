0ad769bb8251c9572f0483c6808bd4c6e6de2768
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2021-02-04 14:12:01 +0100

    Initial commit

---
+++ ConsoleAppJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
